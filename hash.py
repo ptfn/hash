fcd4e2705af787aa2c562633416f467afca7829c
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2019-09-23 22:43:54 +0700

    update

--- hash.py
+++ hash.py
@@ -1,5 +1,5 @@
 import hashlib
-s = input("Введите число ")
+s = input("Введите число или слово ")
 print("Выберите алгоритм хеширования ")
 hash1 = input("md5(m)/SHA(s)")
 if hash1 =='':
@@ -9,7 +9,7 @@ else:
     m = hashlib.md5(str(s).encode("ascii")).hexdigest()
     print ("md5:",m)
   if hash1 == "s":
-    sha = input("sha-1(1)/sha-256(2)/sha-512(3)")
+    sha = input("sha-1(1)/sha-256(2)/sha-512(3)/sha3-384(4)")
     if sha == "1" :
       sha1 = hashlib.sha1(str(s).encode("ascii")).hexdigest()
       print ("sha1:",sha1)
@@ -18,4 +18,7 @@ else:
       print ("sha256:",sha2)
     if sha == "3" :
       sha3 = hashlib.sha512(str(s).encode("ascii")).hexdigest()
+      print ("sha512:",sha3)
+    if sha == "4" :
+      sha3 = hashlib.sha3_384(str(s).encode("ascii")).hexdigest()
       print ("sha384:",sha3)
